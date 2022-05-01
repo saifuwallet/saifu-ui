5ddca3d3bd46f83f87d92281bbb04ed39ff958b9
tengis617 <tengis617@gmail.com>
2022-05-01 17:43:32 +0900

    add stories

--- src/components/Card.stories.tsx
+++ src/components/Card.stories.tsx
@@ -1,44 +1,46 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import Button from './Button';
+import Card from './Card';
 import '../index.css';
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Elements/Button',
-  component: Button,
+  title: 'Elements/Card',
+  component: Card,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-} as ComponentMeta<typeof Button>;
+} as ComponentMeta<typeof Card>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Card> = (args) => <Card {...args} />;
 
 export const Primary = Template.bind({});
 // More on args: https://storybook.js.org/docs/react/writing-stories/args
 Primary.args = {
   primary: true,
-  label: 'Button',
-  text: 'Primary',
+  label: 'Card',
+  children: 'Primary',
   variant: 'primary',
 };
 
-export const Inverse = Template.bind({});
-Inverse.args = {
-  label: 'Button',
-  text: 'Inverse',
-  variant: 'inverse',
+export const Highlight = Template.bind({});
+Highlight.args = {
+  label: 'Card',
+  text: 'Highlight',
+  variant: 'highlight',
+  children: 'Highlight',
 };
 
 export const Large = Template.bind({});
 Large.args = {
   size: 'lg',
-  label: 'Button',
-  text: 'Large',
+  label: 'Card',
+  children: 'Large',
 };
 
 export const Small = Template.bind({});
 Small.args = {
   size: 'sm',
-  label: 'Button',
+  label: 'Card',
   text: 'Small',
+  children: 'Small',
 };
